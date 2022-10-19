99c2dbea5b3bee77564afcf7745006ee6dd465a7
andreafinottoTelebit <71780093+andreafinottoTelebit@users.noreply.github.com>
2022-10-19 11:54:25 +0200

    configured aspnetrunbasics project with serilog

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 
 namespace AspnetRunBasics
 {
@@ -11,7 +13,33 @@ namespace AspnetRunBasics
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host
+                .CreateDefaultBuilder(args)
+                // For integrating Serilog, we are going to edit the default builder which implements the default aspnet logger
+                .UseSerilog(
+                    (context, configuration) =>
+                    {
+                        configuration
+                            .Enrich.FromLogContext()
+                            .Enrich.WithMachineName()
+                            .WriteTo.Console()
+                            .WriteTo.Elasticsearch(
+                                new Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions(
+                                    new Uri(context.Configuration["ElasticConfiguration:Uri"])
+                                )
+                                {
+                                    IndexFormat = $"applogs-{context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+                                    AutoRegisterTemplate = true,
+                                    NumberOfShards = 2,
+                                    NumberOfReplicas = 1
+                                    // Find out what is the meaning of each of these properties
+                                }
+                            )
+                            .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                            .ReadFrom.Configuration(context.Configuration)
+                            ;
+                    }
+                )
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
